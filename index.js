648f6da0266bcf0d13b921874f616ba5c0be64b4
JenLim <cmdlhz@gmail.com>
2021-05-14 00:11:04 +0900

    #2.4 Events and event handlers

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert('hello');
+const title = document.querySelector('#title');
+
+function handleResize(e){
+  console.log(e);
+}
+
+window.addEventListener('resize', handleResize);
+
+title.addEventListener('click', function(){
+  console.log('hello');
+});
